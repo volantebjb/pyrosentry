293a8ef1d89de8e8177f21ffd4a296d259cb08a7
volantebjb <volantebjb@proton.me>
2022-08-06 08:43:00 +0800

    Added Roberts Cross Edge Detection feature

---
+++ src/roberts.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+from skimage.filters import roberts
+
+directory = "../public/dataset"
+raw_dir = os.path.join(directory, "raw")
+roberts_dir = os.path.join(directory, "roberts")
+
+for i, filename in enumerate(os.listdir(raw_dir)):
+    data = f"fluid_flame_{i+1}.jpg"
+    raw_image_path = os.path.join(raw_dir, data)
+    raw_image = cv2.imread(raw_image_path)
+    
+    grayscale_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+    roberts_image = roberts(grayscale_image)
+    filtered_image = cv2.convertScaleAbs(roberts_image, alpha=(255.0))
+    
+    cv2.imwrite(os.path.join(roberts_dir, data), filtered_image)
